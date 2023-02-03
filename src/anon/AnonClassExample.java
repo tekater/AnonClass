0715c234ff29b5829e0515538f09ac9599178ce5
tekater <tekateplay1@yandex.ru>
2023-02-03 19:08:34 +0900

    1.1

--- src/anon/AnonClassExample.java
+++ src/anon/AnonClassExample.java
@@ -1,14 +1,14 @@
 package anon;
 
 public class AnonClassExample {
-    private static String HI_STRING = "Get ready for the next battle!"; // константа static
+    private static String START_ROUND_STRING = "Get ready for the next battle!"; // константа static
     public static void main(String[] args) {
         final String finalRound = "Perfect round!"; // константа final местная переменная
 
         SpeakingEntity tekkenSpeaker = new SpeakingEntity() {
             @Override
             public void sayHello() {
-                System.out.println(HI_STRING);
+                System.out.println(START_ROUND_STRING);
             }
 
             @Override
